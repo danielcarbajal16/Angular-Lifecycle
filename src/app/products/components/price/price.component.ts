748c823796dbf95ce6a7e56c94d150829be5ee53
Eduardo Daniel Carbajal <eduardo.carbajal@jalisco.gob.mx>
2023-12-15 14:02:15 -0600

    Components modified to identify when and how work each lifecycle Angular component

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price',
@@ -6,6 +7,25 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class PriceComponent {
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+  @Input()
+  public price: number = 0;
 
+  public interval$?: Subscription
+  
+  ngOnInit(): void {
+    console.log('Componente HIJO: ngOnInit');
+
+    this.interval$ = interval(1000).subscribe(value => console.log('Tick:', value));
+  }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log({ changes });
+    console.log('Componente HIJO: ngOnChanges');
+  }
+  
+  ngOnDestroy(): void {
+    console.log('Componente HIJO: ngOnDestroy');
+    this.interval$?.unsubscribe();
+  }
 }
