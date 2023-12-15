9ecdb02a79d8a7816d7fcfe8b996c342845809b6
Eduardo Daniel Carbajal <eduardo.carbajal@jalisco.gob.mx>
2023-12-15 12:10:29 -0600

    Initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ProductsModule } from './products/products.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    ProductsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
